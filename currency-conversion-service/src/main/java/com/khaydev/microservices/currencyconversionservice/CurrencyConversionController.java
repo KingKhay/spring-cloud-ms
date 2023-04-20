f50d98659b72cf2e6342f64ae0d9ba7626bf4663
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-20 13:33:23 +0000

    added open feign dependency

---
+++ currency-conversion-service/src/main/java/com/khaydev/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,38 @@
+package com.khaydev.microservices.currencyconversionservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from",from);
+        uriVariables.put("to",to);
+
+        ResponseEntity<CurrencyConversion> response = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion body = response.getBody();
+
+        CurrencyConversion currencyConversion =
+                new CurrencyConversion(body.getId(),
+                        from,
+                        to,
+                        quantity,
+                        body.getConversionMultiple(),
+                        quantity.multiply(body.getConversionMultiple()),
+                        body.getEnvironment());
+
+        return currencyConversion;
+    }
+}
