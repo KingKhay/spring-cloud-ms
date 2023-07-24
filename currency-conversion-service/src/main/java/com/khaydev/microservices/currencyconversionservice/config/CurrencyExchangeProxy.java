878094e80f3904e4e8064a9fa744ffbca12bc95c
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-24 10:36:05 +0000

    refactor Currency Exchange Proxy

--- currency-conversion-service/src/main/java/com/khaydev/microservices/currencyconversionservice/config/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/khaydev/microservices/currencyconversionservice/config/CurrencyExchangeProxy.java
@@ -1,7 +1,13 @@
 package com.khaydev.microservices.currencyconversionservice.config;
 
+import com.khaydev.microservices.currencyconversionservice.CurrencyConversion;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange(url = "http://currency-exchange")
 public interface CurrencyExchangeProxy {
+
+    @GetExchange("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion getCurrencyExchange(@PathVariable String from, @PathVariable String to);
 }
